36a29e278fd400745fd567143ec5e8cd4041e630
Nicolas Pierrard <npierrard@metamicro.com>
2026-05-07 18:58:20 +0200

    fix: supprimer conflit de routes studio, ajouter middleware redirect

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/studio') {
+    return NextResponse.redirect(new URL('/studio/structure', request.url))
+  }
+}
+
+export const config = {
+  matcher: '/studio',
+}
